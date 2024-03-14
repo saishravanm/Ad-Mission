7e8ec54a8772da895b30045899c0b9447d873928
ferLaika <fedesalvideal@outlook.com>
2024-03-14 09:04:19 -0500

    backend - add payment object

--- backend/Models/paymentmodel.js
+++ backend/Models/paymentmodel.js
@@ -1,8 +1,49 @@
 const mongoose = require("mongoose");
+
 const paymentSchema = mongoose.Schema({
-    cardNumber: {
-        type: Number
+    provider: {
+        type: String,
+        required: true
     },
+    cardNumber: {
+        type: String,
+        required: true
+    },
+    expirationDate: {
+        type: String,
+        required: true
+    },
+    securityCode: {
+        type: String,
+        required: true
+    },
+    cardHolderName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
 });
+
+paymentSchema.pre("save", async function (next) {
+    const payment = this;
+
+    try {
+        // Hash sensitive data before saving
+        const salt = await bcrypt.genSalt(10);
+        if (payment.isModified("cardNumber")) {
+            payment.cardNumber = await bcrypt.hash(payment.cardNumber, salt);
+        }
+        if (payment.isModified("securityCode")) {
+            payment.securityCode = await bcrypt.hash(payment.securityCode, salt);
+        }
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
 
 module.exports = mongoose.model("Payment", paymentSchema);
