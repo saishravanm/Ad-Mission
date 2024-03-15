4e574ca16c0a63189c0aab1891c6f3815b07c718
matthewqiu <qiumatthew@gmail.com>
2024-03-15 13:45:00 -0500

    Create standarduser.js

---
+++ backend/Models/standarduser.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const standarduserSchema = mongoose.Schema({
+    userEmail: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    phoneNumber: {
+        type: Number
+    },
+    addressStreet: {
+        type: String
+    },
+    addressCity: {
+        type: String
+    },
+    addressState: {
+        type: String
+    },
+    addressZIP: {
+        type: Number
+    },
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    birthday: {
+        type: Number
+    }
+
+});
+
+standarduserSchema.pre("save", async function (next) {
+    const user = this;
+    if (!user.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        user.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
