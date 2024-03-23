581da312508e7bc82c4fbadde65a2aef2707f8ce
ferLaika <fedesalvideal@outlook.com>
2024-03-23 10:49:03 -0500

    payment object backend

--- backend/Models/paymentmodel.js
+++ backend/Models/paymentmodel.js
@@ -1,8 +1,40 @@
 const mongoose = require("mongoose");
+
 const paymentSchema = mongoose.Schema({
     cardNumber: {
-        type: Number
+        type: String,
+        required: true
     },
+    expiry: {
+        type: String,
+        required: true
+    },
+    cvv: {
+        type: String,
+        required: true
+    },
+    cardholderName: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    paymentDate: {
+        type: Date,
+        default: Date.now
+    },
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movie',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
 });
 
 module.exports = mongoose.model("Payment", paymentSchema);
