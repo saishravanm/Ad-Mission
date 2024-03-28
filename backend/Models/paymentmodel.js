9602fd1f9aa2dea9da19da19cb65972fbd47a689
ferLaika <fedesalvideal@outlook.com>
2024-03-28 15:25:49 -0500

    input validation added

--- backend/Models/paymentmodel.js
+++ backend/Models/paymentmodel.js
@@ -7,15 +7,33 @@ const paymentSchema = mongoose.Schema({
     },
     cardNumber: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^\d{16}$/.test(v); // Validates if cardNumber is 16 digits
+            },
+            message: props => `${props.value} is not a valid credit card number!`
+        }
     },
     expiry: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /\d{2}\/\d{2}/.test(v); // Validates if expiry is in MM/YY format
+            },
+            message: props => `${props.value} is not a valid expiry date!`
+        }
     },
     cvv: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^\d{3}$/.test(v); // Validates if cvv is 3 digits
+            },
+            message: props => `${props.value} is not a valid CVV!`
+        }
     },
     cardholderName: {
         type: String,
@@ -23,7 +41,8 @@ const paymentSchema = mongoose.Schema({
     },
     amount: {
         type: Number,
-        required: true
+        required: true,
+        min: 0 // Validates if amount is a non-negative number
     },
     paymentDate: {
         type: Date,
